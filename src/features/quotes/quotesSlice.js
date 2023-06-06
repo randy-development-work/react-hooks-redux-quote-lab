eebb2ab301ac1bc32abc37f9e8d8e6a5cd851770
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-06-06 10:55:35 +0300

    Add createSlice alternative to quotesSlice.

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,4 +1,5 @@
 import { v4 as uuid } from "uuid";
+import { createSlice } from "@reduxjs/toolkit";
 
 // Action Creators
 // TODO: Create action creators as defined in tests
@@ -58,3 +59,38 @@ export default function quotesReducer(state = initialState, action) {
       return state;
   }
 }
+
+// // Slice alternative
+// const slice = createSlice({
+//   name: "quotes",
+//   initialState: [],
+//   reducers: {
+//     add: (state, action) => {
+//       state.push(action.payload);
+//     },
+//     remove: (state, action) => {
+//       state = state.filter((quote) => quote.id !== action.payload);
+//     },
+//     upvote: (state, action) => {
+//       state = state.map((quote) =>
+//         quote.id === action.payload
+//           ? { ...quote, votes: quote.votes + 1 }
+//           : quote
+//       );
+//     },
+//     downvote: (state, action) => {
+//       state = state.map((quote) =>
+//         quote.id === action.payload && quote.votes > 0
+//           ? { ...quote, votes: quote.votes - 1 }
+//           : quote
+//       );
+//     },
+//   },
+// });
+
+// // actions
+// const { add, remove, upvote, downvote } = slice.actions
+
+// // exports
+// export { add, remove, upvote, downvote }
+// export default slice.reducer
